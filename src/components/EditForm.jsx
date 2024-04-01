0673f9e244f5f4abcc6a27aa04b9ba574b13768c
srinivasGokarla <srinivasgokarla@gmail.com>
2024-04-02 02:03:21 +0530

    done

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function EditForm({ user, onSave, onCancel }) {
   const [editedUser, setEditedUser] = useState({ ...user });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setEditedUser(prevState => ({
+    setEditedUser((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
@@ -17,26 +17,62 @@ function EditForm({ user, onSave, onCancel }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="edit-form-container">
-    <label>
-      Username:
-      <input type="text" name="username" value={editedUser.username} onChange={handleChange} />
-    </label>
-    <label>
-      Email:
-      <input type="email" name="email" value={editedUser.email} onChange={handleChange} />
-    </label>
-    <label>
-      Phone:
-      <input type="text" name="phone" value={editedUser.phone} onChange={handleChange} />
-    </label>
-    <label>
-      Company URL:
-      <input type="text" name="companyUrl" value={editedUser.companyUrl} onChange={handleChange} />
-    </label>
-    <button type="submit">Save</button>
-    <button type="button" onClick={onCancel}>Cancel</button>
-  </form>
+    <div className="overlay">
+      <form onSubmit={handleSubmit} className="edit-form-container">
+        <div className="modal-header">
+          <h4>Basic Modal</h4>
+          <span type="button" onClick={onCancel} className="close-btn">
+            &#10005;
+          </span>
+        </div>
+        <hr />
+        <label>
+          <span style={{ color: "red" }}>*</span> Name:
+          <input
+            type="text"
+            name="username"
+            value={editedUser.username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <span style={{ color: "red" }}>*</span> Email:
+          <input
+            type="email"
+            name="email"
+            value={editedUser.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <span style={{ color: "red" }}>*</span> Phone:
+          <input
+            type="text"
+            name="phone"
+            value={editedUser.phone}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <span style={{ color: "red" }}>*</span> Website:
+          <input
+            type="text"
+            name="companyUrl"
+            value={editedUser.companyUrl}
+            onChange={handleChange}
+          />
+        </label>
+        <hr />
+        <div className="modal-footer">
+          <button type="button" onClick={onCancel} className="cancel-btn">
+            Cancel
+          </button>
+          <button type="submit" className="save-btn">
+            Ok
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
