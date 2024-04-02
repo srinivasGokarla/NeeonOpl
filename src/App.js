42874c8298a68755d168b06f57bd95e09ddbee2f
srinivasGokarla <srinivasgokarla@gmail.com>
2024-04-02 15:55:54 +0530

    updated the code

--- src/App.js
+++ src/App.js
@@ -13,14 +13,16 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
+
 // Mock JSON data
-const mockUserData = [
+var mockUserData = [
   {
     id: 1,
     username: "Leanne Graham",
     profilePhoto: "https://via.placeholder.com/100",
     email: "Sincere@april.biz",
     phone: "1-770-736-8031 x56442",
+    liked: false,
     companyUrl: "http://hildegard.org",
   },
   {
@@ -29,6 +31,7 @@ const mockUserData = [
     profilePhoto: "https://via.placeholder.com/100",
     email: "Shanna@melissa.tv",
     phone: "010-692-6593 x09125",
+    liked: false,
     companyUrl: "http://anastasia.net",
   },
   {
@@ -37,6 +40,7 @@ const mockUserData = [
     profilePhoto: "https://via.placeholder.com/100",
     email: "Nathan@yesenia.net",
     phone: "1-463-123-4447",
+    liked: false,
     companyUrl: "http://ramiro.info",
   },
   {
@@ -45,6 +49,7 @@ const mockUserData = [
     profilePhoto: "https://via.placeholder.com/100",
     email: "Julianne.OConner@kory.org",
     phone: "493-170-9623 x156",
+    liked: false,
     companyUrl: "http://kale.biz",
   },
   {
@@ -53,6 +58,7 @@ const mockUserData = [
     profilePhoto: "https://via.placeholder.com/100",
     email: "Lucio_Hettinger@annie.ca",
     phone: "(254)954-1289",
+    liked: false,
     companyUrl: "http://demarco.info",
   },
   {
@@ -61,6 +67,7 @@ const mockUserData = [
     profilePhoto: "https://via.placeholder.com/100",
     email: "Karley_Dach@jasper.info",
     phone: "1-477-935-8478 x6430",
+    liked: false,
     companyUrl: "http://ola.org",
   },
   {
@@ -69,6 +76,7 @@ const mockUserData = [
     profilePhoto: "https://via.placeholder.com/100",
     email: "Telly.Hoeger@billy.biz",
     phone: "210.067.6132",
+    liked: false,
     companyUrl: "http://elvis.io",
   },
   {
@@ -77,6 +85,7 @@ const mockUserData = [
     profilePhoto: "https://via.placeholder.com/100",
     email: "Sherwood@rosamond.me",
     phone: "586.493.6943 x140",
+    liked: false,
     companyUrl: "http://jacynthe.com",
   },
   {
@@ -85,6 +94,7 @@ const mockUserData = [
     profilePhoto: "https://via.placeholder.com/100",
     email: "Chaim_McDermott@dana.io",
     phone: "(775)976-6794 x41206",
+    liked: false,
     companyUrl: "http://conrad.com",
   },
   {
@@ -96,36 +106,49 @@ const mockUserData = [
     companyUrl: "http://ambrose.net",
   },
 ];
-
+const localStorageKey = "userData";
+var mockUserData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
 
 function App() {
   const [userData, setUserData] = useState(mockUserData);
   const [editUserId, setEditUserId] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+  useEffect(() => {
+    
+    localStorage.setItem(localStorageKey, JSON.stringify(userData));
+  }, [userData]);
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(userData));
+  }, [userData]);
+
 
   const handleLike = (id) => {
-    setUserData(
-      userData.map((user) => {
-        if (user.id === id) {
-          return { ...user, liked: !user.liked };
-        }
-        return user;
-      })
+    const updatedUserData = userData.map((user) =>
+      user.id === id ? { ...user, liked: !user.liked } : user
     );
+    setUserData(updatedUserData);
+    updateMockData(updatedUserData);
   };
 
   const handleEdit = (id) => {
     setEditUserId(id);
   };
 
+ 
+
   const handleSaveEdit = (editedUser) => {
-    setUserData(
-      userData.map((user) => {
-        if (user.id === editedUser.id) {
-          return editedUser;
-        }
-        return user;
-      })
+    const updatedUserData = userData.map((user) =>
+      user.id === editedUser.id ? editedUser : user
     );
+    setUserData(updatedUserData);
+    updateMockData(updatedUserData);
     setEditUserId(null);
   };
 
@@ -134,69 +157,84 @@ function App() {
   };
 
   const handleDelete = (id) => {
-    setUserData(userData.filter((user) => user.id !== id));
+    const updatedUserData = userData.filter((user) => user.id !== id);
+    setUserData(updatedUserData);
+    updateMockData(updatedUserData);
+  };
+  const updateMockData = (updatedUserData) => {
+    mockUserData = [...updatedUserData];
   };
 
   return (
+   
     <div className="App">
-      <ul className="user-list">
-        {userData.map((user) => (
-          <li key={user.id}>
-            <div className="img-holder">
-              <img
-                src="https://avatars.dicebear.com/v2/avataaars/Bret.svg?options[mood][]=happy"
-                alt="dp"
-              />
-            </div>
-            <div className="info-div">
-              <p className="name"> {user.username}</p>
-              <p>
-                <FontAwesomeIcon icon={faEnvelope} className="mail-icon" />{" "}
-                {user.email}
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faPhoneFlip} className="call-icon" />{" "}
-                {user.phone}
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faGlobe} className="web-icon" />{" "}
-                {user.companyUrl}
-              </p>
-            </div>
-            <div className="parent-btn-div">
-              <div className="button-div">
-                <div onClick={() => handleLike(user.id)}>
-                  {user.liked ? (
-                    <FontAwesomeIcon icon={solidHeart} color="red" />
-                  ) : (
-                    <FontAwesomeIcon icon={faHeart} />
-                  )}
-                </div>
-                <div className="height-lines"></div>
-                <FontAwesomeIcon
-                  icon={faPen}
-                  size="1x"
-                  onClick={() => handleEdit(user.id)}
-                  className="edit-icon"
-                />
-                <div className="height-lines"></div>
-                <FontAwesomeIcon
-                  icon={faTrash}
-                  onClick={() => handleDelete(user.id)}
-                  className="delete-icon"
+      {loading ? (
+       <div className="loading-indicator">
+       <p className="loading-text"></p>
+     </div>
+      ) : (
+        <ul className="user-list">
+          {userData.map((user) => (
+            <li key={user.id}>
+              <div className="img-holder">
+                <img
+                  src="https://avatars.dicebear.com/v2/avataaars/Bret.svg?options[mood][]=happy"
+                  alt="dp"
                 />
               </div>
-            </div>
-            {editUserId === user.id && (
-              <EditForm
-                user={user}
-                onSave={handleSaveEdit}
-                onCancel={handleCancelEdit}
-              />
-            )}
-          </li>
-        ))}
-      </ul>
+              <div className="info-div">
+                <p className="name"> {user.username}</p>
+                <p>
+                  <FontAwesomeIcon icon={faEnvelope} className="mail-icon" />{" "}
+                  {user.email}
+                </p>
+                <p>
+                  <FontAwesomeIcon
+                    icon={faPhoneFlip}
+                    className="call-icon"
+                  />{" "}
+                  {user.phone}
+                </p>
+                <p>
+                  <FontAwesomeIcon icon={faGlobe} className="web-icon" />{" "}
+                  {user.companyUrl}
+                </p>
+              </div>
+              <div className="parent-btn-div">
+                <div className="button-div">
+                  <div onClick={() => handleLike(user.id)}>
+                    {user.liked ? (
+                      <FontAwesomeIcon icon={solidHeart} color="red" />
+                    ) : (
+                      <FontAwesomeIcon icon={faHeart} />
+                    )}
+                  </div>
+                  <div className="height-lines"></div>
+                  <FontAwesomeIcon
+                    icon={faPen}
+                    size="1x"
+                    onClick={() => handleEdit(user.id)}
+                    className="edit-icon"
+                  />
+                  <div className="height-lines"></div>
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    onClick={() => handleDelete(user.id)}
+                    className="delete-icon"
+                  />
+                </div>
+              </div>
+              {editUserId === user.id && (
+                <EditForm
+                  user={user}
+                  onSave={handleSaveEdit}
+                  onCancel={handleCancelEdit}
+                />
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
